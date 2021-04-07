e7696b764712342c2982819855e74c5a951719dc
SophieeV <sophievesin@wanadoo.fr>
2021-04-07 11:05:19 +0200

    ajout fichiers

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+let movies = ['Le Silence des agneaux', 'Orange Mécanique', 'Shinning'];
+
+
+function btn1() {
+  document.getElementById('btn1');
+alert(movies);
+}
+
+
+
+function btn(){
+  document.getElementById('btn')
+  alert ('' + 'ajouté au tableau')
+}
+//appuyer sur le bouton ajouter (btn) la value de l'input doit s'ajouter au tableau
