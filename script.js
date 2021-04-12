43814c4924fd2c767cf1b1d9267bf5883e0abea3
SophieeV <sophievesin@wanadoo.fr>
2021-04-12 15:55:13 +0200

    corrections

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 
-
 let movies = ['Le Silence des agneaux', 'Orange Mécanique', 'Shinning'];
-
 
 function btn1() {
   document.getElementById('btn1');
 alert(movies);
 }
 
-
-
 function btn(){
-  let NewMovie = document.getElementById('input');
-  alert (NewMovie + 'ajouté au tableau')
-  console.log
+  let NewMovie = document.getElementById('input').value;
+  movies.unshift(NewMovie);
+  alert (NewMovie + ' ajouté au tableau')
+
 }
 //appuyer sur le bouton ajouter (btn) la value de l'input doit s'ajouter au tableau
